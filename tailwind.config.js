2cd7200e81cc509d05d3a544139f4d373ef7eb33
Worakarn Oun <skboardgames@gmail.com>
2021-11-01 18:51:32 +0700

    Import styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,21 @@ module.exports = {
       screens: {
         'print': { 'raw': 'print' }
       },
+      fontFamily: {
+        'sans_english': ['Inter', 'sans-serif'],
+        'head': ['NotoSerifDisplay', 'serif'],
+      },
       colors: {
         app_white: '#F7FBFE',
         app_dark: '#333333',
-        skeletons: '#E5E7EB',
+        skeletons: '#e4e4e4',
+        primary_pink:'#E26A9A',
+        primary_blue:'#274CD7',
+        primary_yellow:'#CD8B0F',
+        primary_orange:'#C44F34',
         typo: {
           main: '#2D2D2D',
-          minor: '#CBCBCB',
+          minor: '#6E7278',
         },
       },
     },
