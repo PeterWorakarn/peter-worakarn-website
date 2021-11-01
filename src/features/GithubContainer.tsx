d0f770b7a3290dd4e285929adc1a8c4c87c7138f
Worakarn Oun <skboardgames@gmail.com>
2021-11-01 18:52:45 +0700

    Create GithubContainer.tsx

---
+++ src/features/GithubContainer.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const GithubContainer: React.FC = () => {
+  const [state, setstate] = useState('')
+
+  // Defining async function
+  async function getReadMeapi(name: string, project_name: string): Promise<void> {
+    const api_url = `https://raw.githubusercontent.com/${name}/${project_name}/master/README.md`;
+
+    // Storing response
+    const response = await fetch(api_url);
+
+    // Storing data in form of JSON
+    const data = await response.text();
+    setstate(data);
+    // return data;
+  }
+
+  useEffect(() => {
+    getReadMeapi('PeterWorakarn', 'PeterWorakarn');
+  }, [])
+
+  return (
+    <>Github</>
+  )
+}
+
+export default GithubContainer
