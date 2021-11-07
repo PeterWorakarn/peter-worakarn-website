59cf25433e64a863d187797c20a0f5da5e7b11b4
Worakarn Oun <skboardgames@gmail.com>
2021-11-07 21:19:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true
+  },
   reactStrictMode: true,
   images: {
+    formats: ['image/avif', 'image/webp'],
     domains: ['avatars.githubusercontent.com', 'peter.datayolk.net', 'datayolk.net', 'dummyimage.com'], // TODO: configure real domains
   },
 }
