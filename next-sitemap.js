3c60d85ed7aa421ad5ddfd9b143a20c91ca3a0c8
Worakarn Oun <skboardgames@gmail.com>
2021-11-10 21:53:35 +0700

    Add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,12 @@
+const siteUrl = "https://www.capsules.com";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${siteUrl}/sitemap.xml`,
+      `${siteUrl}/server-sitemap.xml`,
+    ],
+  },
+};
