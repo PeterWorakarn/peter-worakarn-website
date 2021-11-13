7b19661bce282eae5e34ada4778e60f2ca78bcd9
Worakarn Oun <skboardgames@gmail.com>
2021-11-13 23:56:47 +0700

    Create custom error page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next'
 
-const NotFound: NextPage = () => {
+const ServerFail: NextPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-center text-4xl sm:text-6xl tracking-wider leading-tight sm:leading-none my-5 text-white font-sans_english">
-        <span className="font-light text-9xl">404</span> <br /> Page <i className="not-italic custom_underline">Not Found</i>
+        <span className="font-light text-9xl">500</span> <br /> Server <i className="not-italic custom_underline">Error</i>
       </h1>
     </div>
   )
 }
 
-export default NotFound;
+export default ServerFail;
