f203a0a082adc1ce230163a27570ca1cd37bc428
Worakarn Oun <skboardgames@gmail.com>
2021-12-05 04:05:31 +0700

    [Misc] Temporary Disable Client Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import dynamic from 'next/dynamic';
 import { NextSeo } from 'next-seo';
 
 const BioContainer = dynamic(import('../features/BioContainer'), {ssr: true});
-const GithubContainer = dynamic(import('../features/GithubContainer'), {ssr: false});
-const DatayolkContainer = dynamic(import('../features/DatayolkContainer'), {ssr: false});
+// const GithubContainer = dynamic(import('../features/GithubContainer'), {ssr: false});
+// const DatayolkContainer = dynamic(import('../features/DatayolkContainer'), {ssr: false});
 
 export async function getStaticProps() {
   const res = await fetch('https://peter.datayolk.net/peter-bio')
@@ -42,8 +42,8 @@ const Home: NextPage<HomeProps> = ({ Biodata }) => {
     <div className="flex flex-col gap-40">
       <NextSeo {...SEO} />
       <BioContainer data={Biodata} />
-      <GithubContainer />
-      <DatayolkContainer />
+      {/* <GithubContainer /> */}
+      {/* <DatayolkContainer /> */}
     </div>
   )
 }
