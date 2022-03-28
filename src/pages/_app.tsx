64204297910f85e3d27c7390d13d219dd54ffba2
Worakarn Oun <skboardgames@gmail.com>
2022-03-28 10:28:53 +0700

    Example of using Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { initializeMoment } from '../features/utils/moment';
-
+import { useEffect, useState } from 'react';
+import * as ga from 'react-ga'
+import { useRouter } from 'next/dist/client/router';
+import { gaTrackingCode } from '../configs';
 import dynamic from 'next/dynamic';
 
 const HeadSEO = dynamic(import('../features/components/HeadSEO'), { ssr: false });
@@ -10,8 +13,24 @@ const Providers = dynamic(import('../features/Providers'), { ssr: false });
 
 
 initializeMoment();
+ga.initialize(gaTrackingCode);
 
 function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    // Run Logic on GA
+    ga.pageview(window.location.pathname);
+    router.events.on('routeChangeStart', () => {
+      setIsLoading(true);
+    });
+
+    router.events.on('routeChangeComplete', () => {
+      setIsLoading(false);
+      console.log('Fire PageViews')
+      ga.pageview(window.location.pathname);
+    });
+  }, []);
 
   return (
     <Providers>
