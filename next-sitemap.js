2dd2d86b24758d3ee91a325691877083812f0615
Worakarn Oun <skboardgames@gmail.com>
2022-08-30 12:38:44 +0700

    update sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,6 +3,7 @@ const siteUrl = "peter-o.tech";
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  generateIndexSitemap: true,
   robotsTxtOptions: {
     additionalSitemaps: [
       `${siteUrl}/sitemap.xml`,
